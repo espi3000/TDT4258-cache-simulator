7ca35f121cde6b48ff1a9866c28544b0bd2065b8
espi3000 <espen.holsen.eh@gmail.com>
2022-09-27 21:18:07 +0200

    Finished cache simulator

--- cache_sim.c
+++ cache_sim.c
@@ -4,10 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-//#define get_tag_mask(num_tag_bits)                  (0xffffffff << (32 - num_tag_bits))
-//#define get_index_mask(num_offset_bits, tag_mask)   ((0xffffffff << num_offset_bits) & ~tag_mask)
-//#define get_offset_mask(tag_mask, index_mask)       (~(tag_mask | index_mask))
-
 typedef enum {dm, fa} cache_map_t;
 typedef enum {uc, sc} cache_org_t;
 typedef enum {instruction, data} access_t;
@@ -20,39 +16,80 @@ typedef struct {
 typedef struct {
     uint64_t accesses;
     uint64_t hits;
-    // You can declare additional statistics if
-    // you like, however you are now allowed to
-    // remove the accesses or hits
 } cache_stat_t;
 
-// DECLARE CACHES AND COUNTERS FOR THE STATS HERE
 uint32_t cache_size;
 uint32_t block_size = 64;
 cache_map_t cache_mapping;
 cache_org_t cache_org;
 
-// USE THIS FOR YOUR CACHE STATISTICS
-cache_stat_t cache_statistics;
+typedef struct {
+    uint32_t tag;
+    uint8_t valid;
+} block_t;
 
+typedef struct {
+    block_t* blocks;
+} cache_t;
 
 /**
- * TODO: Will use strtok() instead of strsep() if possible.
- * @brief Slices a string. strsep() is not standard C. This is Chris Dodd's implementation. Source:
- *        https://stackoverflow.com/questions/58244300/getting-the-error-undefined-reference-to-strsep-with-clang-and-mingw
- * @param stringp address of string
- * @param delim separator character
- * @return address of first string
+ * @brief Checks cache for a hit and updates cache
+ * 
+ * @param cache 
+ * @param tag 
+ * @param index 
+ * @return 1 if hit, 0 if miss
  */
-char* strsep(char** stringp, const char* delim) {
-    char* rv = *stringp;
-    if (rv) {
-        *stringp += strcspn(*stringp, delim);
-        if (**stringp)
-            *(*stringp)++ = '\0';
-        else
-            *stringp = 0; 
+uint8_t check_block_dm(cache_t cache, uint32_t tag, uint32_t index) {
+    if (cache.blocks[index].valid == 0) {
+        cache.blocks[index].tag = tag;
+        cache.blocks[index].valid = 1;
+    } else if (cache.blocks[index].tag == tag) {
+        return 1;
+    } else {
+        cache.blocks[index].tag = tag;  // This causes segmentation fault
     }
-    return rv;
+    return 0;
+}
+
+/**
+ * @brief Checks cache for a hit and updates cache
+ * 
+ * @param cache 
+ * @param tag 
+ * @param num_blocks 
+ * @return 1 if hit, 0 if miss
+ */
+uint8_t check_block_fa(cache_t cache, uint32_t tag, uint8_t num_blocks) {
+    static uint8_t first_in_index = 0;
+    for (uint8_t i = 0; i < num_blocks; i++) {
+        if (cache.blocks[i].valid == 0) {
+            cache.blocks[i].tag = tag;
+            cache.blocks[i].valid = 1;
+            break;
+        } else if (cache.blocks[i].tag == tag) {
+            return 1;
+        } else if (i == num_blocks - 1) {
+            cache.blocks[first_in_index].tag = tag;
+            first_in_index++;
+            if (first_in_index == num_blocks) first_in_index = 0;
+        }
+    }
+    return 0;
+}
+
+/**
+ * @brief Opens file in read mode and returns a pointer to the file
+ * 
+ * @param filename path of file with file extension
+ * @return FILE* pointer to file
+ */
+FILE* open_file(const char* filename) {
+    FILE* ptr_file = fopen(filename, "r");
+    if (!ptr_file) {
+        printf("Unable to open the trace file\n");
+        exit(1);
+    }
 }
 
 /* 
@@ -79,9 +116,17 @@ mem_access_t read_transaction(FILE* ptr_file) {
     return access;
 }
 
-void main(int argc, char** argv) {
-    // Reset statistics:
-    memset(&cache_statistics, 0, sizeof(cache_stat_t));
+void main(int argc, char** argv) { // argv[0] is program name, parameters start with argv[1]
+    uint8_t num_blocks;
+    uint8_t num_offset_bits;
+    uint8_t num_index_bits;
+    uint8_t num_tag_bits;
+    uint32_t tag_mask;
+    uint32_t index_mask;
+    //uint32_t offset_mask;
+
+    cache_t cache;
+    cache_t cache2;
 
     /* 
      * Read command-line parameters and initialize:
@@ -99,10 +144,8 @@ void main(int argc, char** argv) {
             "[cache organization: uc|sc]\n");
         exit(0);
     } else {
-        /* argv[0] is program name, parameters start with argv[1] */
-
-        /* Set cache size */
-        cache_size = atoi(argv[1]);
+        cache_size = atoi(argv[1]);         // Set cache size
+        num_blocks = cache_size/block_size; // Calculate number of blocks
 
         /* Set Cache Mapping */
         if (strcmp(argv[2], "dm") == 0) {
@@ -117,93 +160,76 @@ void main(int argc, char** argv) {
         /* Set Cache Organization */
         if (strcmp(argv[3], "uc") == 0) {
             cache_org = uc;
+            cache.blocks = (block_t*)malloc(num_blocks*sizeof(block_t)); // Allocate memory for blocks
+            for (uint8_t i = 0; i < num_blocks; i++) { // Initialize cache to 0
+                cache.blocks[i].valid = 0;
+                cache.blocks[i].tag = 0;
+            }
         } else if (strcmp(argv[3], "sc") == 0) {
             cache_org = sc;
+            num_blocks /= 2;
+            cache.blocks = (block_t*)malloc(num_blocks*sizeof(block_t)); // Allocate memory for blocks
+            cache2.blocks = (block_t*)malloc(num_blocks*sizeof(block_t)); // Allocate memory for blocks
+            for (uint8_t i = 0; i < num_blocks; i++) { // Initialize cache to 0
+                cache.blocks[i].valid = 0;
+                cache.blocks[i].tag = 0;
+                cache2.blocks[i].valid = 0;
+                cache2.blocks[i].tag = 0;
+            }
         } else {
             printf("Unknown cache organization\n");
             exit(0);
         }
     }
 
-    // ============================================================
-    // ============================================================
-    uint8_t num_blocks = cache_size/64; // Blocks are 64B
-    uint8_t num_index_bits = 0;
-    for (; num_blocks > 1; num_index_bits++) num_blocks >>= 1; // Calculates log2 of 2^integer
-    uint8_t num_offset_bits = 6;
-    uint8_t num_tag_bits = 32 - num_offset_bits - num_index_bits; // 32 b addr, 6 b offset (64 B blocks)
+    /* Calculate number of bits for offset, index and tag */
+    num_offset_bits = 6;        // 2^6 = 64 bytes per block
+    num_index_bits = 0;         // Calculates log2 of 2^num_blocks to calculate index bits
+    for (uint8_t temp = num_blocks; temp > 1; num_index_bits++) temp >>= 1;
+    num_tag_bits = 32 - num_offset_bits - num_index_bits; // Always 32 bit address
 
-    uint32_t tag_mask = (0xffffffff << (32 - num_tag_bits));
-    uint32_t index_mask = (0xffffffff << num_offset_bits) & ~tag_mask;
-    uint32_t offset_mask = ~(tag_mask | index_mask);
+    /* Calculate bitmasks*/
+    tag_mask = (0xffffffff << (32 - num_tag_bits));
+    index_mask = (0xffffffff << num_offset_bits) & ~tag_mask;
+    //offset_mask = ~(tag_mask | index_mask);
 
-    typedef struct {
-        uint32_t tag;
-        uint8_t valid;
-        //uint8_t dirty;
-        //uint8_t lru;
-    } block_t;
-
-    typedef struct {
-        block_t blocks[num_blocks];
-    } cache_t;
-
-    cache_t cache;
-    memset(&cache, 0, sizeof(cache_t));
-    // ============================================================
-    // ============================================================
-
-    /* Open the file mem_trace.txt to read memory accesses */
-    FILE* ptr_file;
-    ptr_file = fopen("mem_trace1.txt", "r");
-    if (!ptr_file) {
-        printf("Unable to open the trace file\n");
-        exit(1);
-    }
-
+    FILE* ptr_file = open_file("mem_trace2.txt"); // Read memory trace file
     mem_access_t access;
+    cache_stat_t cache_statistics;
+    memset(&cache_statistics, 0, sizeof(cache_stat_t)); // Reset statistics:
     while (1) { // Loop until whole trace file has been read
         access = read_transaction(ptr_file);
-        cache_statistics.accesses++;
         if (access.address == 0) break; // If no transactions left, break out of loop
-        printf("%d %x\n", access.accesstype, access.address);
-        /* Do a cache access */
-
-        uint8_t index;
-        uint8_t tag;
-        switch (cache_mapping) {
-        case dm:
+        cache_statistics.accesses++;
+        //printf("%d %x\n", access.accesstype, access.address);
+        uint32_t index;
+        uint32_t tag;
+        if (cache_mapping == dm) {
             index = (access.address & index_mask) >> num_offset_bits;
             tag = (access.address & tag_mask) >> (num_offset_bits + num_index_bits);
-            if (cache.blocks[index].valid == 0) {
-                cache.blocks[index].tag = tag;
-                cache.blocks[index].valid = 1;
-            } else if (cache.blocks[index].tag == tag) {
-                cache_statistics.hits++;
+            if (cache_org == uc) {
+                cache_statistics.hits += check_block_dm(cache, tag, index);
             } else {
-                cache.blocks[index].tag = tag;
-            }
-            break;
-        case fa:
-            tag = (access.address & (tag_mask | index_mask)) >> num_offset_bits;
-            for (uint8_t i = 0; i < num_blocks; i++) {
-                if (cache.blocks[i].valid == 0) {
-                    cache.blocks[i].tag = tag;
-                    cache.blocks[i].valid = 1;
-                    break;
-                } else if (cache.blocks[i].tag == tag) {
-                    cache_statistics.hits++;
-                    break;
+                if (access.accesstype == data) {
+                    cache_statistics.hits += check_block_dm(cache, tag, index);
                 } else {
-                    cache.blocks[i].tag = tag;
-                    break;
+                    cache_statistics.hits += check_block_dm(cache2, tag, index);
                 }
             }
-            break;
+        } else {
+            tag = (access.address & (tag_mask | index_mask)) >> num_offset_bits;
+            if (cache_org == uc) {
+                cache_statistics.hits += check_block_fa(cache, tag, num_blocks);
+            } else {
+                if (access.accesstype == data) {
+                    cache_statistics.hits += check_block_fa(cache, tag, num_blocks);
+                } else {
+                    cache_statistics.hits += check_block_fa(cache2, tag, num_blocks);
+                } 
+            }
         }
     }
 
-    /* Print the statistics */
     // DO NOT CHANGE THE FOLLOWING LINES!
     printf("\nCache Statistics\n");
     printf("-----------------\n\n");
@@ -212,8 +238,8 @@ void main(int argc, char** argv) {
     printf("Hit Rate: %.4f\n",
          (double)cache_statistics.hits / cache_statistics.accesses);
     // DO NOT CHANGE UNTIL HERE
-    // You can extend the memory statistic printing if you like!
 
-    /* Close the trace file */
-    fclose(ptr_file);
+    fclose(ptr_file);       // Close the trace file
+    free(cache.blocks);     // Free the allocated memory
+    free(cache2.blocks);    // Free the allocated memory
 }
